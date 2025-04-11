5377ffda8b066e957fcba526b05c22ddeddb8897
sushantpoudel <poudelsushant72@gmail.com>
2025-04-11 19:53:16 +0545

    second commit

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById('login-form')?.addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Login form submitted!');
+});
+
+document.getElementById('register-form')?.addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Registration form submitted!');
+});
+
+document.getElementById('ride-form')?.addEventListener('submit',function(event){
+    event.preventDefault();
+    alert('Ride booked');
+
+});
+
